9fb0643b51d543cbc51f2aa400f00e5e42befa24
Yusuf Faraji <yusuf_faraji@33922.gnm.int>
2019-02-01 14:50:26 +0000

    Remove println

--- app/model/GooglePushMessage.scala
+++ app/model/GooglePushMessage.scala
@@ -7,10 +7,7 @@ import play.api.libs.json.{Format, Json}
 case class GooglePushMessageWrapper(message: GooglePushMessage, subscription: String)
 
 case class GooglePushMessage(attributes: Map[String, String], data: String, messageId: String) {
-  val decodedData = {
-    println(data)
-    new String(Base64.getDecoder.decode(data))
-  }
+  val decodedData = new String(Base64.getDecoder.decode(data))
 }
 
 object GooglePushMessageWrapper {
