f3e24b60cd1d2629f79dad15a764154e2ae1b2a1
Yusuf Faraji <yusuf_faraji@33922.gnm.int>
2019-02-01 15:19:09 +0000

    Add deserialization test for incoming messages

--- test/model/GooglePushMessageSpec.scala
+++ test/model/GooglePushMessageSpec.scala
@@ -1,5 +1,32 @@
 package model
 
-class GooglePushMessageSpec {
+import java.util.Base64
+
+import fixtures.TestFixtures
+import org.scalatest.{Matchers, WordSpecLike}
+import play.api.libs.json.Json
+
+class GooglePushMessageSpec extends WordSpecLike with Matchers {
+
+    "Google push message" must {
+      "deserialise correctly" in {
+        val base64Str = new String(Base64.getEncoder.encode(
+          Json.asciiStringify(Json.toJson(TestFixtures.developerNotificationWithSubscription)).getBytes)
+        )
+
+        val jsonStr = s"""{
+                        |  "message" : {
+                        |    "attributes" : {},
+                        |    "data" : "$base64Str",
+                        |    "messageId" : "messageId"
+                        |  },
+                        |  "subscription" : "subscription"
+                        |}""".stripMargin
+
+        val json = Json.fromJson[GooglePushMessageWrapper](Json.parse(jsonStr))
+
+        TestFixtures.googlePushMessageWrapper shouldBe json.get
+      }
+    }
 
 }
