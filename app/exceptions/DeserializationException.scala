cb00aff8d609502543a154ab3e1b4a635f159066
Yusuf Faraji <yusuf_faraji@33922.gnm.int>
2019-02-05 12:08:34 +0000

    Update message to contain json errors

--- app/exceptions/DeserializationException.scala
+++ app/exceptions/DeserializationException.scala
@@ -3,11 +3,16 @@ package exceptions
 import play.api.libs.json.{JsPath, JsonValidationError}
 
 case class DeserializationException(message: String, errors: Seq[(JsPath, Seq[JsonValidationError])])
-  extends Exception(message)
+  extends Exception(message) {
+}
 
 
 object DeserializationException {
   def apply(message: String): DeserializationException = {
-    DeserializationException(message, Seq.empty)
+    new DeserializationException(message, Seq.empty)
+  }
+
+  def apply(message: String, errors: Seq[(JsPath, Seq[JsonValidationError])]): DeserializationException = {
+    new DeserializationException(s"$message with Json Errors: $errors", errors)
   }
 }
