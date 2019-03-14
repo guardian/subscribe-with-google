6f63ffb12d74f6cb70cbdfadabd113a53ad921ba
Yusuf Faraji <yusuf.faraji@gmail.com>
2019-03-14 20:45:56 +0000

    Add in ignore to sqs for retry mechanism

--- app/queue/SQSProvider.scala
+++ app/queue/SQSProvider.scala
@@ -10,7 +10,7 @@ import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
 import com.amazonaws.services.sqs.model.{Message, QueueDoesNotExistException}
 import com.amazonaws.services.sqs.{AmazonSQSAsync, AmazonSQSAsyncClientBuilder}
 import config.CredentialProvider
-import exceptions.DeserializationException
+import exceptions.{DeserializationException, IgnorableException}
 import javax.inject.Inject
 import javax.inject.Singleton
 import model.GooglePushMessageWrapper
@@ -20,6 +20,7 @@ import play.api.libs.json.Json
 import routing.MessageRouter
 
 import scala.collection.JavaConverters._
+import scala.concurrent.Future
 import scala.concurrent.duration._
 
 trait SQSListener
@@ -29,6 +30,8 @@ class SQSListenerImpl @Inject()(messageRouter: MessageRouter,
                                 credentialProvider: CredentialProvider,
                                 config: Configuration)(implicit system: ActorSystem, materializer: Materializer)
     extends SQSListener {
+
+  private implicit val ec = system.dispatcher
 
   private val queueUrl = config.get[String]("sqs.queue-url")
   private val sqsRegion = config.get[String]("sqs.region")
@@ -55,6 +58,7 @@ class SQSListenerImpl @Inject()(messageRouter: MessageRouter,
       f
     })
     .via(Flow.fromFunction(handleMessage))
+    .mapAsync(1)(f => f)
     .toMat(sink)(Keep.right)
 
   RestartSource
@@ -67,7 +71,7 @@ class SQSListenerImpl @Inject()(messageRouter: MessageRouter,
     }
     .runWith(Sink.ignore)
 
-  private def handleMessage(message: Message) = {
+  private def handleMessage(message: Message): Future[MessageAction] = {
     val parseMessage = () => {
       Json.parse(message.getBody)
         .validate[GooglePushMessageWrapper]
@@ -77,9 +81,16 @@ class SQSListenerImpl @Inject()(messageRouter: MessageRouter,
         }
     }
 
-    messageRouter.handleMessage(parseMessage)
-
-    MessageAction.Delete(message)
+    messageRouter.handleMessage(parseMessage).map{
+      case Left(e: IgnorableException) =>
+        logger.info(s"Ignorable exception encountered ${e.getMessage}")
+        MessageAction.Delete(message)
+      case Left(e) =>
+        logger.error("Failure to complete payment :: This is a recoverable error - placing message back on queue", e)
+        MessageAction.Ignore(message)
+      case Right(_) =>
+        MessageAction.Delete(message)
+    }
   }
 
   private def queueExists(queueUrl: String): Unit = {
