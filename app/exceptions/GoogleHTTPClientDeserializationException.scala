38864d724befd12103170aa33d016f3e15ba8ecc
Yusuf Faraji <yusuf.faraji@gmail.com>
2019-03-15 12:22:43 +0000

    Add deserialization execeptions for google client

--- app/exceptions/GoogleHTTPClientDeserializationException.scala
+++ app/exceptions/GoogleHTTPClientDeserializationException.scala
@@ -4,3 +4,15 @@ import play.api.libs.json.{JsPath, JsonValidationError}
 
 case class GoogleHTTPClientDeserializationException(message: String, errors: Seq[(JsPath, Seq[JsonValidationError])])
     extends Exception(message)
+
+
+object GoogleHTTPClientDeserializationException {
+  def apply(message: String): GoogleHTTPClientDeserializationException = {
+    new GoogleHTTPClientDeserializationException(message, Seq.empty)
+  }
+
+  def apply(message: String, errors: Seq[(JsPath, Seq[JsonValidationError])]): GoogleHTTPClientDeserializationException = {
+    new GoogleHTTPClientDeserializationException(s"$message with Json Errors: $errors", errors)
+  }
+
+}
